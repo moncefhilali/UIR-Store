e79636f53e36dff03f99e8f8cd8f3d2e224ab7ca
douniya99 <douniya.elmaniti@um5r.ac.ma>
2023-11-16 15:57:30 +0100

    entities

---
+++ server-asp/Domain/Entities/CustomerOrders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("_CustomerOrders", Schema = "dbo")]
+
+    public class CustomerOrders : BaseEntity
+    {
+        [Column("ClientID")]
+        public int ClientID { get; set; }
+
+        [Column("productId")]
+        public int productId { get; set; }
+
+        [Column("qtyOrded")]
+        public int qtyOrded { get; set; }
+
+        [Column("OrderDate")]
+        public DateTime OrderDate { get; set; }
+
+        [Column("TotalPrice")]
+        public int TotalPrice { get; set; }
+
+    }
+}
---
+++ server-asp/Domain/Entities/ProductOwners.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domain.Entities
+{
+
+    [Table("_ProductOwners", Schema = "dbo")]
+
+    public class ProductOwners : BaseEntity    {
+        [Column("UserId")]
+        public int UserId { get; set; }
+
+    }
+}
